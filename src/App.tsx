3c0144af547887df9f3bcd5c4d15ff2a27ab536a
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-08-18 14:16:15 +0500

    feature: added module and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import Questionnaire from "./pages/questionnaire/questionnaire";
+import Settings from "./pages/settings/settings";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route path="/" component={Questionnaire} exact />
+      <Route path="/settings" component={Settings} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/configs/questions.tsx
@@ -0,0 +1,9 @@
+export const questionsData = [
+  {
+    category: "personal",
+    question: "What's your age?",
+    type: "number",
+    min: 18,
+    max: 120,
+  },
+];
