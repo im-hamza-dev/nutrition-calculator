745efe9fe36b94d703d9cd905a300670a5aa4663
im-hamza-dev <hi.hamza.dev@gmail.com>
2024-08-22 03:15:27 +0500

    feature: added pdf editor tool

---
+++ src/components/summary/help.js
@@ -0,0 +1,375 @@
+import { degrees, PDFDocument, rgb, StandardFonts } from "pdf-lib";
+import PdfFile from "../../assets/files/template.pdf";
+
+export const generatePdf = async (
+  answers,
+  calculateCalories,
+  macros,
+  handRules
+) => {
+  const {
+    weight,
+    height: personHeight,
+    age,
+    targetWeight,
+    targetDate,
+    gender,
+    exerciseLevel,
+    eatingStyle,
+    meals,
+    goals,
+  } = answers;
+
+  const existingPdfBytes = await fetch(PdfFile).then((res) =>
+    res.arrayBuffer()
+  );
+
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
+
+  const pages = pdfDoc.getPages();
+  const thirdPage = pages[2];
+  const fourthPage = pages[3];
+  const fifthPage = pages[4];
+  const { width, height } = thirdPage.getSize();
+  const backgroundColor = rgb(0.9, 0.9, 0.9); // Light grey background color
+  const backgroundColorWhite = rgb(1, 1, 1); // Light grey background color
+  const blue = rgb(0.1, 0.7, 0.8); // Light grey background color
+  //age
+  thirdPage.drawRectangle({
+    x: 65,
+    y: 460,
+    width: 105,
+    height: 30,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(JSON.stringify(age), {
+    x: 100,
+    y: 470,
+    size: 25,
+    font: helveticaFont,
+    opacity: 0.6,
+
+    color: rgb(0, 0, 0),
+  });
+
+  //sex
+  thirdPage.drawRectangle({
+    x: 190,
+    y: 460,
+    width: 105,
+    height: 30,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(gender, {
+    x: 210,
+    y: 470,
+    size: 25,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+  //weight
+  thirdPage.drawRectangle({
+    x: 315,
+    y: 460,
+    width: 105,
+    height: 30,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(JSON.stringify(weight) + " kg", {
+    x: 350,
+    y: 470,
+    size: 25,
+    font: helveticaFont,
+    opacity: 0.6,
+
+    color: rgb(0, 0, 0),
+  });
+
+  //height
+  thirdPage.drawRectangle({
+    x: 440,
+    y: 460,
+    width: 105,
+    height: 30,
+    color: backgroundColor,
+  });
+  let hgt = `${personHeight.toFixed()} cm`;
+  thirdPage.drawText(hgt, {
+    x: 465,
+    y: 470,
+    size: 25,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //exercise level
+  let exerciseBarWidth = 120;
+  if (exerciseLevel === "Light") {
+    exerciseBarWidth = 170;
+  } else if (exerciseLevel === "Moderate") {
+    exerciseBarWidth = 220;
+  } else if (exerciseLevel === "Instense") {
+    exerciseBarWidth = 250;
+  } else if (exerciseLevel === "Very Instense") {
+    exerciseBarWidth = 290;
+  }
+  thirdPage.drawRectangle({
+    x: 185,
+    y: 415,
+    width: exerciseBarWidth,
+    height: 21,
+    color: blue,
+  });
+  thirdPage.drawText(exerciseLevel, {
+    x: 200,
+    y: 422,
+    size: 13,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //eating style
+  thirdPage.drawRectangle({
+    x: 185,
+    y: 300,
+    width: 160,
+    height: 25,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(eatingStyle, {
+    x: 200,
+    y: 310,
+    size: 13,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //meals
+  thirdPage.drawRectangle({
+    x: 460,
+    y: 300,
+    width: 80,
+    height: 25,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(JSON.stringify(meals), {
+    x: 490,
+    y: 310,
+    size: 18,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //goal
+  thirdPage.drawRectangle({
+    x: 110,
+    y: 130,
+    width: 155,
+    height: 30,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(goals, {
+    x: 120,
+    y: 143,
+    size: 17,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+  //target-weight
+  thirdPage.drawRectangle({
+    x: 315,
+    y: 130,
+    width: 105,
+    height: 30,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(JSON.stringify(targetWeight), {
+    x: 350,
+    y: 143,
+    size: 17,
+    font: helveticaFont,
+    opacity: 0.6,
+
+    color: rgb(0, 0, 0),
+  });
+
+  //target-date
+  thirdPage.drawRectangle({
+    x: 440,
+    y: 130,
+    width: 105,
+    height: 30,
+    color: backgroundColor,
+  });
+  thirdPage.drawText(targetDate, {
+    x: 445,
+    y: 143,
+    size: 17,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //fourth page:
+  let protienLineA = `${handRules.palm} palm-sized portions (or ${macros.protien} g)`;
+  //protien
+  fourthPage.drawRectangle({
+    x: 140,
+    y: 635,
+    width: 300,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(protienLineA, {
+    x: 150,
+    y: 638,
+    size: 15,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  let protienLineB = `That's ${(handRules.palm / meals).toFixed()} palm or ${(
+    macros.protien / meals
+  ).toFixed()} g of protien per meal`;
+  fourthPage.drawRectangle({
+    x: 140,
+    y: 570,
+    width: 300,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(protienLineB, {
+    x: 150,
+    y: 580,
+    size: 12,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //carbs
+  let carbsLineA = `${handRules.handful} cupped handfuls (or ${macros.carbs} g)`;
+  fourthPage.drawRectangle({
+    x: 140,
+    y: 407,
+    width: 300,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(carbsLineA, {
+    x: 150,
+    y: 413,
+    size: 15,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  let carbsLineB = `That's ${(handRules.handful / meals).toFixed()} palm or ${(
+    macros.carbs / meals
+  ).toFixed()} g of protien per meal`;
+  fourthPage.drawRectangle({
+    x: 140,
+    y: 350,
+    width: 300,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(carbsLineB, {
+    x: 150,
+    y: 360,
+    size: 12,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //fats
+  let fatsLineA = `${handRules.thumb} thumb-sized portions (or ${macros.fats} g)`;
+  fourthPage.drawRectangle({
+    x: 140,
+    y: 290,
+    width: 300,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(fatsLineA, {
+    x: 150,
+    y: 293,
+    size: 15,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  let fatsLineB = `That's ${(handRules.thumb / meals).toFixed()} palm or ${(
+    macros.fats / meals
+  ).toFixed()} g of protien per meal`;
+  fourthPage.drawRectangle({
+    x: 140,
+    y: 230,
+    width: 300,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(fatsLineB, {
+    x: 150,
+    y: 240,
+    size: 12,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //calories
+  let caloriesLine = `Eating like this will provide the ${calculateCalories?.requiredCalories} calories you are estimated to need to reach your goals`;
+  fourthPage.drawRectangle({
+    x: 60,
+    y: 170,
+    width: 500,
+    height: 30,
+    color: backgroundColorWhite,
+  });
+  fourthPage.drawText(caloriesLine, {
+    x: 60,
+    y: 180,
+    size: 11,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+
+  //meals
+  fifthPage.drawRectangle({
+    x: 470,
+    y: 655,
+    width: 60,
+    height: 30,
+    color: backgroundColor,
+  });
+  fifthPage.drawText(JSON.stringify(meals), {
+    x: 480,
+    y: 665,
+    size: 14,
+    font: helveticaFont,
+    opacity: 0.6,
+    color: rgb(0, 0, 0),
+  });
+  console.log(width, height);
+
+  const pdfBytes = await pdfDoc.save();
+  const url_ = window.URL.createObjectURL(new Blob([pdfBytes]));
+  const link = document.createElement("a");
+  link.href = url_;
+  link.setAttribute("download", "file.pdf"); //or any other extension
+  document.body.appendChild(link);
+  link.click();
+};
--- src/components/summary/summary.component.tsx
+++ src/components/summary/summary.component.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { QuestionStates } from "../../context/questionsProvider";
 import { calculateCalories, daysUntilDeadline } from "../../utils/helper";
-import { degrees, PDFDocument, rgb, StandardFonts } from "pdf-lib";
 // @ts-ignore
-import PdfFile from "../../assets/files/template.pdf"
 import "./summary.scss";
+import { generatePdf } from "./help";
 const nutritionData = [
   {
     id: 1,
@@ -44,6 +43,12 @@ const Summary = () => {
     carbs: 0,
     fats: 0,
   });
+  const [handRules, setHandRules] = useState<any>({
+    palm: 0,
+    handful: 0,
+    thumb: 0,
+  });
+
   const [macrosRatio, setMacrosRatio] = useState<any>({
     protien: 0,
     carbs: 0,
@@ -80,9 +85,21 @@ const Summary = () => {
     let calorieData_: any = calculateCalories(answers);
     setCaloriesData(calorieData_);
     let macros_ = macros;
-    macros_.protien = (calorieData_?.requiredCalories * ratio.protien) / 100;
-    macros_.carbs = (calorieData_?.requiredCalories * ratio.carbs) / 100;
-    macros_.fats = (calorieData_?.requiredCalories * ratio.fats) / 100;
+    macros_.protien = Math.floor(
+      (calorieData_?.requiredCalories * ratio.protien) / 100 / 4
+    );
+    macros_.carbs = Math.floor(
+      (calorieData_?.requiredCalories * ratio.carbs) / 100 / 4
+    );
+    macros_.fats = Math.floor(
+      (calorieData_?.requiredCalories * ratio.fats) / 100 / 9
+    );
+    let handRules_ = handRules;
+    handRules_.palm = Math.floor(macros_.protien / 30);
+    handRules_.handful = Math.floor(macros_.carbs / 30);
+    handRules_.thumb = Math.floor(macros_.fats / 12);
+    setHandRules(handRules_);
+    setMacros(macros_);
   }, [answers]);
   const getMacroRatio = (label: string) => {
     let ratio = "30% Protien, 35% Carb, 35% Fat";
@@ -95,33 +112,7 @@ const Summary = () => {
     }
     return ratio;
   };
-  const generatePdf = async () => {
-    const existingPdfBytes = await fetch(PdfFile).then((res) => res.arrayBuffer());
 
-    const pdfDoc = await PDFDocument.load(existingPdfBytes);
-    const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-    const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    const { width, height } = firstPage.getSize();
-    firstPage.drawText("This text sadfsafdswas added with JavaScript!", {
-      x: 5,
-      y: height / 2 + 300,
-      size: 50,
-      font: helveticaFont,
-      color: rgb(0.95, 0.1, 0.1),
-      rotate: degrees(-45),
-    });
-    console.log(width, firstPage)
-
-    const pdfBytes = await pdfDoc.save();
-    const url_ = window.URL.createObjectURL(new Blob([pdfBytes]));
-    const link = document.createElement('a');
-    link.href = url_;
-    link.setAttribute('download', 'file.pdf'); //or any other extension
-    document.body.appendChild(link);
-    link.click();
-  };
   return (
     <div className="questionWrapper">
       <p>(Brief summary below)</p>
@@ -158,42 +149,48 @@ const Summary = () => {
       <div className="summary-macros">
         <h2>HERE'S HOW THAT BREAKS DOWN:</h2>
         <div className="summary-macros-item">
-          Protien: {Math.floor(macros.protien / 4)} g
+          Protien: {Math.floor(macros.protien)} g
         </div>
         <div className="summary-macros-item">
-          Carbs: {Math.floor(macros.carbs / 4)} g
+          Carbs: {Math.floor(macros.carbs)} g
         </div>
         <div className="summary-macros-item">
-          Fat: {Math.floor(macros.fats / 9)} g
+          Fat: {Math.floor(macros.fats)} g
         </div>
       </div>
       <div className="nutrition-info">
-        {nutritionData.map((item) => (
-          <div key={item.id} className="nutrition-item">
+        {Object.keys(macros).map((item) => (
+          <div className="nutrition-item">
             <div className="nutrition-image">
-              <img src={item.image} alt={item.type} />
+              {/* <img src={item.image} alt={item.type} /> */}
             </div>
             <div className="nutrition-details">
               <h3>
-                {item.portions} (or {item.weight}) of{" "}
-                <span className={item.colorClass}>{item.type}</span> per day
+                {item === "protien" &&
+                  `${handRules.palm} palm sized portions ( or ${macros[item]} g ) \n of Protien per day`}
+                {item === "carbs" &&
+                  `${handRules.handful} cupped handfuls ( or ${macros[item]} g ) \n of Carbohydrates per day`}
+                {item === "fats" &&
+                  `${handRules.thumb} thumb sized portions ( or ${macros[item]} g ) \n of Healthy Fats per day`}
               </h3>
-              <p>
-                That's <strong>{item.total}</strong>.
-              </p>
-              <div className="nutrition-examples">
+              <p>{/* That's <strong>{item.total}</strong>. */}</p>
+              {/* <div className="nutrition-examples">
                 <strong>FOR EXAMPLE</strong>
                 <ul>
                   {item.examples.map((example, index) => (
                     <li key={index}>{example}</li>
                   ))}
                 </ul>
-              </div>
+              </div> */}
             </div>
           </div>
         ))}
       </div>
-      <button onClick={generatePdf}>Generate</button>
+      <button
+        onClick={() => generatePdf(answers, caloriesData, macros, handRules)}
+      >
+        Generate
+      </button>
     </div>
   );
 };
